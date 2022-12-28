508f62da1cdec0b1ee09e362712c15861a2e0063
Alberto Marques Nunes <alberto.nunes@primesolutions.com.br>
2022-12-28 19:49:30 -0300

    Custom Service Leval authorization

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
@@ -155,4 +155,15 @@ public class User implements UserDetails, Serializable {
 		
 		return true;
 	}
+	
+	public boolean hasHole(String roleName) {
+		for(Role role : roles) 
+		{
+			if(role.getAuthority().equals(roleName)) {
+				return true;
+		   }
+		}
+		return false;
+	}
 }
+
